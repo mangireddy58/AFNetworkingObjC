d13fcaad4b9585192372c3579cddd7748e01acef
saipraveenkumar <sai_katteboina@simplicity-creations.com>
2017-07-07 15:49:53 +0530

    Initial Commit

---
+++ ObjCDelegate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCDelegate
+//
+//  Created by Rushikesh Kulkarni on 07/07/17.
+//  Copyright © 2017 simplicity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
